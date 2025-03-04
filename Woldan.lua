96905e28683e2178e8a22fb87cd4ec95efc1314d
Woldan1337 <woldancontact@gmail.com>
2025-03-04 16:28:52 +0300

    Woldan.lua

--- Woldan.lua
+++ Woldan.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Woldan1337/drakplon1243/refs/heads/main/Gamelist.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
