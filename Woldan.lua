5186d366427c06a4f740ef9b909cbd12744e7ca0
Woldan1337 <woldancontact@gmail.com>
2025-05-09 10:40:46 +0300

    Delete Woldan.lua

--- Woldan.lua
+++
@@ -1,7 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Woldan1337/drakplon1243/refs/heads/main/WoldanXD.lua"))()
-
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-    end
-end
